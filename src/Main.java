fbdf8b2491cd84532740d820e0f26f2e2c20d212
Dima <dmitriy.matveev89@inbox.ru>
2022-11-18 12:03:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+//        Максимальный числа для следующих типов
+        System.out.println("max for byte: " + Byte.MAX_VALUE);
+        System.out.println("max for short: " + Short.MAX_VALUE);
+        System.out.println("max for int: " + Integer.MAX_VALUE);
+        System.out.println("max for long: " + Long.MAX_VALUE);
+
+        System.out.println("max for float: " + Float.MAX_VALUE);
+        System.out.println("max for double: " + Double.MAX_VALUE);
+
+//        Минимальные числа для следующих типов
+        System.out.println("min for Byte: " + Byte.MIN_VALUE);
+        System.out.println("min for short: " + Short.MIN_VALUE);
+        System.out.println("min for int: " + Integer.MIN_VALUE);
+        System.out.println("min for long: " + Long.MIN_VALUE);
+
+        System.out.println("min for float: " + Float.MIN_VALUE);
+        System.out.println("min for double: " + Double.MIN_VALUE);
+    }
+}
